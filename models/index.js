ec0bf7ea66b155dd19b246566542daa22ecf5442
Harris Nadeem <harrisnadeem444@gmail.com>
2023-11-18 22:34:15 +0500

    Added Relations

--- models/index.js
+++ models/index.js
@@ -4,13 +4,31 @@ const category = require("../models/schemas/categorySchema");
 const product = require("../models/schemas/productSchema");
 const sequelize = require("../common/dbconnection");
 
-user.hasOne(cart, {
+user.hasMany(cart, {
   onDelete: "CASCADE",
-  foreignKey: { name: "userID", allowNull: false, unique: true },
+  foreignKey: { name: "userId", allowNull: false, unique: true },
 });
 cart.belongsTo(user, {
   onDelete: "CASCADE",
-  foreignKey: { name: "userID", allowNull: false, unique: true },
+  foreignKey: { name: "userId", allowNull: false, unique: true },
+});
+
+category.hasMany(product, {
+  onDelete: "CASCADE",
+  foreignKey: { name: "categoryId", allowNull: false, unique: true },
+});
+product.belongsTo(category, {
+  onDelete: "CASCADE",
+  foreignKey: { name: "categoryId", allowNull: false, unique: true },
+});
+
+product.belongsToMany(cart, {
+  through: "product_cart",
+  foreignKey: { name: "productId" },
+});
+cart.belongsToMany(product, {
+  through: "product_cart",
+  foreignKey: { name: "cartId" },
 });
 
 const models = sequelize.models;
